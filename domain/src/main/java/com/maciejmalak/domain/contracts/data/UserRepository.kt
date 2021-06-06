fde22dd69c6499999c4a4834b7ecdead8abd7392
Maciej Malak <maciej.malak@malak.com>
2021-06-06 23:13:24 +0200

    Adds contract/api for a data layer

--- domain/src/main/java/com/maciejmalak/domain/contracts/data/UserRepository.kt
+++ domain/src/main/java/com/maciejmalak/domain/contracts/data/UserRepository.kt
@@ -1,5 +1,16 @@
 package com.maciejmalak.domain.contracts.data
 
-interface UserRepository {
+import com.maciejmalak.domain.models.User
 
+/*
+A contract doesn't reveal implementation details of a data source behind it.
+Methods names are simple, yet universal.
+ */
+interface UserRepository {
+    fun put(vararg users: User): User
+    fun getAll(): List<User>
+    fun get(vararg users: User): User
+    fun deleteAll(): Boolean
+    fun delete(vararg users: User): Boolean
+    fun update(vararg users: User): List<User>
 }
