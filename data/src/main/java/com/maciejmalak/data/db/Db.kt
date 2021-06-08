34214cb55913e4d6cd1388fbdc96641966e3d6f7
Maciej Malak <maciej.malak@malak.com>
2021-06-08 20:02:37 +0200

    Makes singleton out of DB instance. Kotlin object declaration is thread safe and constructed lazily

--- data/src/main/java/com/maciejmalak/data/db/Db.kt
+++ data/src/main/java/com/maciejmalak/data/db/Db.kt
@@ -1,9 +1,23 @@
 package com.maciejmalak.data.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [User::class], version = 1)
 abstract class Db : RoomDatabase() {
     abstract fun usersDao(): UserDao
+
+    object DbInitializer {
+        private const val DB_NAME = "application_database"
+        private var instance: Db? = null
+
+        fun getInstance(context: Context): Db =
+            instance ?: buildDB(context).also { instance = it }
+
+
+        private fun buildDB(context: Context): Db =
+            Room.databaseBuilder(context, Db::class.java, DB_NAME).build()
+    }
 }
